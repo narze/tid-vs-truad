1371e509a15fd2956b0931fb2292aa3e7a4f5b0d
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-11 04:56:02 +0700

    style: Change Ko-fi text

--- src/lib/Kofi.svelte
+++ src/lib/Kofi.svelte
@@ -26,8 +26,8 @@
       name,
       {
         type: "floating-chat",
-        "floating-chat.donateButton.text": "Support me",
-        "floating-chat.donateButton.background-color": "#00b9fe",
+        "floating-chat.donateButton.text": "สนับสนุนค่า ☕️",
+        "floating-chat.donateButton.background-color": "#ff3e00",
         "floating-chat.donateButton.text-color": "#fff",
       },
       "kofiContainer"
