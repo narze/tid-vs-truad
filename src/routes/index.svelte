6b4c1b278e64522788e0eb52a176fcb4f5d95e76
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-11 04:17:01 +0700

    chore: Remove unused files

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -166,27 +166,4 @@
   * {
     font-family: "Anakotmai";
   }
-
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex: 1;
-  }
-
-  .welcome {
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
-
-  .welcome img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    display: block;
-  }
 </style>
